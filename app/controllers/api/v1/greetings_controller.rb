5c14e91cab9eeeafd2989f9c42cda36c75803a5c
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-02 02:40:40 +0530

    fix linter errors

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,11 +1,9 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
     @greetings = Greeting.all
-    @greetings = @greetings.map do |greeting|
-      greeting.greeting
-    end
+    @greetings = @greetings.map(&:greeting)
     @greetings = @greetings.sample
     sleep 1
-    render json: {greeting: @greetings}
+    render json: { greeting: @greetings }
   end
 end
