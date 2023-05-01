9cb831a5186d146c0382e13919bb5729bc129915
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-02 02:31:30 +0530

    add routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      get 'greetings', to: 'greetings#index'
+    end
+  end
 end
