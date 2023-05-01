d994e580e81ac1c0430100e7b2f1628e2f510379
rbhatt199924 <rbhatt199924@gmail.com>
2023-05-02 02:33:22 +0530

    add greeting and style

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,7 @@
 // Entry point for the build script in your package.json
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-function App() {
-  return (<h1>Hello World!</h1>);
-}
+import App from './components/App';
 
 ReactDOM.render(
   <App/>,
